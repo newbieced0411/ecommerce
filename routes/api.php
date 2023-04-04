20e9a5700a0ba21b77a704e0d7c0b73d460d676b
cedrick <jctdelacarcel@gmail.com>
2023-04-04 22:55:16 +0800

    user api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user();    
+});
+
+Route::controller(UserController::class)->prefix('user')->group(function () {
+    Route::get('/register', 'new');
+    Route::post('/login', 'login');
 });
